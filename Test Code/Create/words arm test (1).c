12250849dcc3e80ef1531fd63ff0b9b66992fb0d
Bob-Ross07 <128514351+Bob-Ross07@users.noreply.github.com>
2023-06-01 12:46:40 -0400

    Update words arm test (1).c

--- Test Code/Create/words arm test (1).c
+++ Test Code/Create/words arm test (1).c
@@ -1,25 +1,29 @@
 //servos
-int claw = 0;
+int claw = 2;
 int arm = 1;
+int shoulder = 0;
 
 //tower grab values
-int high;
-int low;
-int open = 1200;
-int galClose = 20;
-int cubeClose = 50;
+int open = 1900;
+int galClose = 950;
+int cubeClose = 1144;
 
-int highReady;
-int lowReady;
+//arm values
+int high = 213;
+int low = 1826;
 int placeReady;
 
+//shoulder values
+int lTilt  = 1646;
+int pull = 1504;
+int hTilt = 1928; 
+int tuck = 1350;
 //motors
 int base = 0;
 int baseSpeed = 200;
-float armDeg = 3.89;
-
-int tower = 5;
-int stack = 90;
+float armDeg = 5.4;
+int tower = 90;
+int stack = 15;
 
 //touch sensor for calibration
 int set = 0;
@@ -84,13 +88,15 @@ void place() {
   msleep(100);
   
 }
-void highGrab() {
+void highGrab(int object) {
   enable_servo(arm);
   enable_servo(claw);
+  enable_servo(shoulder);
   int speed = 3;
   int armPosition = get_servo_position(arm);
   int basePosition = gmpc(base);
-  while(basePosition != tower || armPosition != highReady){
+  int shoulderPosition = get_servo_position(shoulder);
+  while(basePosition != tower || armPosition != high || shoulderPosition != tuck){
     grabReadyUnlooped(1);
   }
   set_servo_position(claw, open);
@@ -103,46 +109,27 @@ void highGrab() {
     set_servo_position(claw, galClose);   
   }msleep(200);
 }
-void lowGrab() {
+void lowGrab(int object) {
   enable_servo(arm);
   enable_servo(claw);
-  while(basePosition != tower || armPosition != lowReady){
+  while(basePosition != tower || armPosition != low || shoulderPosition != tuck){
     grabReadyUnlooped(0);
   }
   set_servo_position(claw, open);
   msleep(200);
-  set_servo_position(arm, lowGrab);
+  set_servo_position(shoulder, lTilt);
   msleep(200);
   set_servo_position(claw, cubeClose);
   msleep(200);
+  set_servo_position(shoulder, lPull);
 }
-//0 is low tower, 1 is high tower
-void grabReady(int height){
-  enable_servo(arm);
-  enable_servo(claw);
-  int speed = 3;
-  int armPosition = get_servo_position(arm);
-  int basePosition = gmpc(base);
-    if(height == 0){
-        while(basePosition != tower || armPosition != lowReady){
-            grabReadyUnlooped(0);
-            msleep(1);
-        }
-  set_servo_position(claw, open);
-  msleep(200);
-    }
-  if(height == 1){
-      while(basePosition != tower || armPosition != highReady){
-        grabReadyUnlooped(1);
-        msleep(1);
-  }
-}
+
 //maybe use for moving arm as create switches towers    
 void grabReadyUnlooped(int height){
   enable_servo(arm);
   enable_servo(claw);
   int speed = 3;
-  int armPosition = get_servo_position(arm);
+  int shoulderPosition = get_servo_position(shoulder);
   int basePosition = gmpc(base);
     if(height == 0){
         if(basePosition < tower*armDeg){
@@ -153,13 +140,21 @@ void grabReadyUnlooped(int height){
             mtp(base, baseSpeed, basePosition); 
         }else {
             freeze(base);
-        }if(armPosition < lowReady){
+        }if(armPosition < low){
             armPosition += 1;
             set_servo_position(armPosition);
             msleep(speed);
-        }else if(armPosition > lowReady){
+        }else if(armPosition > low){
             armPosition += 1;
             set_servo_position(armPosition);
+            msleep(speed);
+        }}if(shoulderPosition < tuck){
+            armPosition += 1;
+            set_servo_position(shoulderPosition);
+            msleep(speed);
+        }else if(shoulderPosition > tuck){
+            armPosition += 1;
+            set_servo_position(shoulderPosition);
             msleep(speed);
         }
     }
@@ -172,14 +167,22 @@ void grabReadyUnlooped(int height){
       mtp(base, baseSpeed, basePosition); 
     }else {
       freeze(base);
-    }if(armPosition < highReady){
-      armPosition += 1;
-      set_servo_position(armPosition);
-      msleep(speed);
-    }else if(armPosition > highReady){
-      armPosition += 1;
-      set_servo_position(armPosition);
-      msleep(speed);
-    }
+    }if(armPosition < high){
+            armPosition += 1;
+            set_servo_position(armPosition);
+            msleep(speed);
+        }else if(armPosition > high){
+            armPosition += 1;
+            set_servo_position(armPosition);
+            msleep(speed);
+        }}if(shoulderPosition < tuck){
+            armPosition += 1;
+            set_servo_position(shoulderPosition);
+            msleep(speed);
+        }else if(shoulderPosition > tuck){
+            armPosition += 1;
+            set_servo_position(shoulderPosition);
+            msleep(speed);
+        }
     }
 }
